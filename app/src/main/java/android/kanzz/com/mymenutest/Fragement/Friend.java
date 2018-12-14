e0a76cea27887d419642baa8fce148460162619c
zyc2621854 <879615001@qq.com>
2018-12-14 11:30:41 +0800

    完成选择界面的viewPager滑动碎片功能

--- app/src/main/java/android/kanzz/com/mymenutest/Fragement/Friend.java
+++ app/src/main/java/android/kanzz/com/mymenutest/Fragement/Friend.java
@@ -4,7 +4,7 @@ import android.content.Context;
 import android.kanzz.com.mymenutest.R;
 import android.net.Uri;
 import android.os.Bundle;
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,7 +18,7 @@ public class Friend extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater,@Nullable ViewGroup container,@Nullable Bundle savedInstanceState){
         if(mView==null){
-            mView=inflater.inflate(R.layout.fragment_find,null);
+            mView=inflater.inflate(R.layout.fragment_friend,null);
         }
         return mView;
     }
