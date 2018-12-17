baee44755c1e3e5cb18f20ffcdaba1b40f0f6b97
zyc2621854 <879615001@qq.com>
2018-12-17 22:27:22 +0800

    成功测试horizontal进度条

--- app/src/main/java/android/kanzz/com/mymenutest/Activity1.java
+++ app/src/main/java/android/kanzz/com/mymenutest/Activity1.java
@@ -2,12 +2,39 @@ package android.kanzz.com.mymenutest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
 
-public class Activity1 extends AppCompatActivity {
+public class Activity1 extends BaseActivity implements View.OnClickListener {
+
+    private Button progressButton;
+    private ProgressBar mProgressBar;
+
+    private void initView(){
+        mProgressBar=(ProgressBar)findViewById(R.id.progress_bar_horizontal);
+        progressButton=(Button)findViewById(R.id.progress_button);
+        progressButton.setOnClickListener(this);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_1);
+        initView();
+    }
+
+    @Override
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.progress_button:
+                ShowToast("+++++");
+                int progress=mProgressBar.getProgress();
+                progress=progress+10;
+                if(progress<=100){
+                    mProgressBar.setProgress(progress);
+                }
+                break;
+        }
     }
 }
